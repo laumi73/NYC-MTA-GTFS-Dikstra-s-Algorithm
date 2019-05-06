82a4762f7a1f82fcc6ea2f26981abc0282bb5a7d
laumichael73 <laumichael73@gmail.com>
2019-05-06 00:58:40 -0400

    First Commit - Alpha version

---
+++ src/StationListCreator.java
@@ -0,0 +1,15 @@
+
+
+public class StationListCreator {
+    //Fields
+    //Hardcode this bit in
+    String prefix = "C:\\Users\\laumi\\IdeaProjects\\NYC MTA GTFS Dikstra\\src\\";
+
+    //Constructors
+
+    //Methods
+    public static void main(String[] args) {
+
+    }
+
+}
