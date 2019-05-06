27f02193bad63bc60ae5db686eb488f7f3f00e49
laumichael73 <laumichael73@gmail.com>
2019-05-06 16:52:26 -0400

    Deprecated stuffs related to parent station
    added dist and prev for dikstra's algorithm, along with their getters and setters
    
    added addRoute() for functioning with ArrayList

--- src/SubStation.java
+++ src/SubStation.java
@@ -1,21 +1,56 @@
+import java.util.ArrayList;
+
 public class SubStation extends Station {
     //Fields
-    private ParentStation parentStation;
+    //private ParentStation parentStation;
 
-    private Station nextStation;
+    private ArrayList<String> routeList;
+
+    private SubStation prev;
+
+    private int dist;
 
     //Constructors
+    public SubStation() {
+        routeList = new ArrayList<String>();
+        prev = null;
+        dist = -1;
+    }
 
+    public SubStation(SubStation prev, int dist) {
+        routeList = new ArrayList<String>();
+        this.prev = prev;
+        this.dist = dist;
+    }
 
     //Methods
         //Getters
+    /*
     public Station getParentStation() {
         return parentStation;
     }
-
-    public Station getNextStation() {
-        return nextStation;
+    */
+    public SubStation getPrev() {
+        return prev;
     }
 
+    public int getDist() {
+        return dist;
+    }
+        //Setters
+    public void setPrev(SubStation prev) {
+        this.prev = prev;
+    }
 
+    public void setDist(int d) {
+        this.dist = d;
+    }
+
+    public ArrayList<String> getRouteList() {
+        return routeList;
+    }
+
+    public void addRoute(String route) {
+        routeList.add(route);
+    }
 }
