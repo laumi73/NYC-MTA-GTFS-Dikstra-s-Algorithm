6490b7abd80714eecf71e56745353a2cb0e54bbd
laumichael73 <laumichael73@gmail.com>
2019-05-07 06:11:24 -0400

    Removed leftover comments
    added shortest route version of Dikstra's Algorithm

--- src/Dikstra.java
+++ src/Dikstra.java
@@ -49,34 +49,55 @@ public class Dikstra {
             nodeList.remove(currentNodeIndex);
             refSet.remove(currentNodeIndex);
         }
+        return trace(visitedNodes, visitedStations, visitedStations.indexOf(new SubStation(target)));
+    }
 
-        /*
-        while (!(nodeList.isEmpty())) {
-            currentNodeIndex = dist.indexOf(Collections.min(dist));
-            currentNode = vertexSet.get(currentNodeIndex);
-            nodeList.remove(currentNode);
-            //visited.add(currentNode);
-            ArrayList<String> routeList = currentNode.getRouteList();
+    public static String findShortestRoute(ArrayList<SubStation> vertexSet,
+                                          String source,
+                                          String target)
+    {
+        ArrayList<SubStation> visitedStations = new ArrayList<SubStation>(vertexSet.size());
+        ArrayList<SubStationNode> visitedNodes = new ArrayList<SubStationNode>(vertexSet.size());
+        ArrayList<SubStation> refSet = new ArrayList<SubStation>(vertexSet.size());
+        ArrayList<SubStationNode> nodeList = new ArrayList<SubStationNode>(vertexSet.size());
 
+        for (int i = 0; i < vertexSet.size(); i++) {
+            refSet.add(vertexSet.get(i));
+            nodeList.add(new SubStationNode(Double.POSITIVE_INFINITY, null, vertexSet.get(i)));
+        }
+
+        int currentNodeIndex = refSet.indexOf(new SubStation(source)); //Might use this later
+        nodeList.get(currentNodeIndex).setDist(0);
+        SubStationNode currentNode;
+        //Loop until the nodeList is empty (all nodes visited)
+        while (!nodeList.isEmpty()) {
+            //Set the "cursor" as the node with the least dist
+            currentNode = Collections.min(nodeList);
             //Go through the list of routes
+            ArrayList<String> routeList = currentNode.getSubStation().getRouteList();
             for (int i = 0; i < routeList.size(); i++) {
                 //Separate the String line into {String destination , String (int) cost}
                 String[] data = routeList.get(i).split(",");
 
-                //Find which index is the destination SubStation
-                int destIndex = vertexSet.indexOf(new SubStation(data[0]));
-
-                //Calculate cost
-                //Parsed cost + current station cost
-                double cost = Double.parseDouble(data[1]) + dist.get(currentNodeIndex);
-                if (cost < dist.get(destIndex)) {
-                    dist.set(destIndex, cost);
-                    prev.set(destIndex, currentNode);
+                //Check if it has been visited yet
+                if (!visitedStations.contains(new SubStation((data[0])))) {
+                    //Find which index is the destination SubStation
+                    int destIndex = refSet.indexOf(new SubStation(data[0]));
+                    //Calculate cost
+                    //Parsed cost + current station cost
+                    double cost = 1 + currentNode.getDist();
+                    if (cost < nodeList.get(destIndex).getDist()) {
+                        nodeList.get(destIndex).setDist(cost);
+                        nodeList.get(destIndex).setPrev(currentNode);
+                    }
                 }
             }
+            visitedNodes.add(currentNode);
+            visitedStations.add(currentNode.getSubStation());
+            currentNodeIndex = nodeList.indexOf(currentNode);
+            nodeList.remove(currentNodeIndex);
+            refSet.remove(currentNodeIndex);
         }
-        */
-
         return trace(visitedNodes, visitedStations, visitedStations.indexOf(new SubStation(target)));
     }
 
